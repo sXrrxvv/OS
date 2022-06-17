87939f021c0f99214a01eeb9c5c0654925ace58f
sXrrxvv <104932524+sXrrxvv@users.noreply.github.com>
2022-06-18 01:48:07 +0300

    Update message.h

--- lab4/message.h
+++ lab4/message.h
@@ -5,27 +5,11 @@ private:
 public:
     message() : msg() {;}
 
-    explicit message(const std::string &s) {
-        if(s.size() > 20)
-            throw std::length_error("bad capacity");
-        std::copy(s.begin(), s.end(), msg);
-    }
+    char* getMessage();
 
-    char* getMessage() {return msg;}
+    friend std::fstream& operator << (std::fstream &buf, const message& msg);
 
-    friend std::fstream& operator << (std::fstream &buf, const message& msg) {
-        buf.write((const char*)&msg , sizeof(msg));
-        return buf;
-    }
+    friend std::fstream& operator >> (std::fstream &ifs, message &message);
 
-    friend std::fstream& operator >> (std::fstream &ifs, message &message)
-    {
-        ifs.read((char*)& message, sizeof(message));
-        return ifs;
-    }
-
-    friend std::istream& operator>>(std::istream &is, message &msg) {
-        is >> msg.msg;
-        return is;
-    }
+    friend std::istream& operator>>(std::istream &is, message &msg);
 };
